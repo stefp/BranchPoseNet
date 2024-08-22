2582d55fcb2124316ac87e836e6fa4d66ad7effa
Stefano Puliti <stefano.puliti@gmail.com>
2024-08-23 00:01:48 +0200

    Update whorl_pose_detect.py

--- whorl_pose_detect.py
+++ whorl_pose_detect.py
@@ -1,15 +1,18 @@
 ####################################################################################################################################
 # Import libraries
-import os
+import os, glob
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 import io
 import laspy
 from PIL import Image
 import re
 from scipy.spatial import cKDTree
 import concurrent.futures
+import matplotlib
+matplotlib.use('Agg')  # Use Agg backend for non-GUI rendering
+import matplotlib.pyplot as plt
+
 
 import ultralytics
 from ultralytics import YOLO
@@ -17,65 +20,92 @@ from ultralytics import YOLO
 # import my own functions
 from tools import rotate_point_cloud,slice_tree_center_thick_slices,plot_to_image,plot_section_as_image_with_alpha
 from tools import convert_to_real_world,process_file,calculate_angle_at_p2,calculate_distance,pose_detection_tree
-from tools import convert_sections_to_images,process_point_cloud,get_image_size,list_las_laz_files
+from tools import convert_sections_to_images,process_point_cloud,get_image_size
 
 
 ####################################################################################################################################
 # directory to where YOLO temp predictions are stored
-dir_root="C:/Users/stpu/YOLOv5-whorlDetector/test_forest" # path to root folder where .las/.laz files are stored
+dir_root="data" # path to root folder where .las/.laz files are stored
+
 # alpha for plotting
 alpha= 0.5
-# pose model
-my_model="C:/Users/stpu/YOLOv5-whorlDetector/pose_estimation/whorl_pose_n_640/weights/best.pt"
-# minimum distance between whorls (used to remove duplicates in postprocessing)
-min_dist_whorls=0.25 # in m
 
-# define files to process
-files_to_process= list_las_laz_files(dir_root)
+# minimum distance between whorls (used to remove duplicates)
+min_internodal_d= 0.3 # in m (it can be set to something like 0.01 to allow the full output to post-process later)
+
+# pose model
+my_model="whorl_pose_nano_1000px/weights/best.pt"
+
+# label for the column with tree instance unique identifiers
+tree_id_label='treeID' 
+
+# label for the column with semantic labels (non required)
+semantic_label='semantic' 
 
 ####################################################################################################################################
+# files_to_process= list_las_laz_files(dir_root)
+# Find all .las and .laz files in the root folder
+las_files = glob.glob(os.path.join(dir_root, '*.las'))
+laz_files = glob.glob(os.path.join(dir_root, '*.laz'))
+
+# Combine the lists of files
+all_files = las_files + laz_files
+all_files
+
 # Iterate through all *.las/*.laz files in root folder
-for filename in files_to_process:
+for filename in all_files:
     print("Starting pose estimation for all trees in: "+ filename)
 
     ####################################################################################################################################
     # define inner directories 
-    dir_output=dir_root+"/results" # path to where to store the final results
-    dir_pred=dir_root+"/pred_temp" # temp path to where to store the intermediate prediction
+    dir_output = os.path.join(dir_root, "results")  # path to where to store the final results
+    dir_pred = os.path.join(dir_root, "pred_temp")  # temp path to where to store the intermediate prediction
+    dir_temp_imgs = os.path.join(dir_pred, "orig_imgs") # Further subdirectories within 'dir_pred'
+
+    # Ensure directories exist without throwing an error if they already do
+    os.makedirs(dir_pred, exist_ok=True)
+    os.makedirs(dir_output, exist_ok=True)
+    os.makedirs(dir_temp_imgs, exist_ok=True)
     
-    # create directories
-    if not os.path.exists(dir_pred):
-       os.makedirs(dir_pred)
-    if not os.path.exists(dir_output):
-       os.makedirs(dir_output)
-        
     ####################################################################################################################################
     # Read in the forest segmented data
     print("Reading in lidar data........................................................................................................")
-    one_forest=dir_root+"/"+filename
-    las = laspy.read(one_forest)
+    # Read in the forest segmented data
+    las = laspy.read(filename)
     
     # convert to a numpy array 
-    las_np = np.vstack((las.x, las.y, las.z, las.preds_instance_segmentation, las.preds_semantic_segmentation)).transpose()
+    las_np = np.vstack((las.x, las.y, las.z,  getattr(las, tree_id_label),  getattr(las, semantic_label))).transpose()
     
-    # split tree/non-tree
-    trees= las_np[las_np[:,4]==1]
-    non_tree= las_np[las_np[:,4]==0]
-    
-    #print("tree points: "+ str(round(len(trees)/len(las_np)*100))+"%")
-    #print("Non-tree points: "+ str(round(len(non_tree)/len(las_np)*100))+"%")
+    # split tree/non-tree (assumes that the semantic label corresponds to 0 if it's not a tree and 1 if it is a tree)
+    trees= las_np[getattr(las, semantic_label)!=0]
+    non_tree= las_np[getattr(las, semantic_label)==0]
 
     # Now, modifying the loop to use parallel execution
     unique_treeIDs = np.unique(trees[:,3])
     results = []
     
     ####################################################################################################################################
-    # Parallel processing of all trees in a forest
-    print("Parallel processing..........................................................................................................")
+    # Whorl detection in parallel for all trees in the las file
+    print("Whorl detection..............................................................................................................")
+    # Parallel individual tree whorl pose detection
+    unique_treeIDs = np.unique(getattr(las, tree_id_label))
+    results = []
     
     with concurrent.futures.ThreadPoolExecutor(max_workers=3) as executor:
         # Submitting tasks to the executor
-        future_to_treeID = {executor.submit(pose_detection_tree, treeID, trees, non_tree,dir_root, my_model,min_dist_whorls=0.25): treeID for treeID in unique_treeIDs}
+        future_to_treeID = {
+            executor.submit(
+                pose_detection_tree, 
+                treeID=treeID,         # Pass treeID as a keyword argument
+                trees=trees, 
+                non_tree=non_tree, 
+                dir_root=dir_root, 
+                my_model=my_model, 
+                dir_pred=dir_pred, 
+                min_dist_whorls=min_internodal_d  # Pass min_dist_whorls as a keyword argument
+            ): treeID 
+            for treeID in unique_treeIDs
+        }
         
         # Collecting results as they complete
         for future in concurrent.futures.as_completed(future_to_treeID):
@@ -100,6 +130,12 @@ for filename in files_to_process:
     # correct z
     whorld_df.loc[whorld_df['branch_opening_angle'] == 0, 'z'] = whorld_df['world_pz2']
     
+    # remove negative whorls
+    whorld_df= whorld_df[whorld_df['world_pz2']>=0]
+    
+    # write out the output
+    whorld_df.to_csv(dir_output+'/'+os.path.splitext(os.path.basename(filename))[0]+'_'+str(round(min_internodal_d*100))+'cmThresh_whorls_pc_HKL2model.csv', index=False)  
+  
     
     ####################################################################################################################################
     # Write outputs
